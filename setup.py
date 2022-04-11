93fbab3ea70f05a5eb915c75101771d503f9555d
Anson Chan <94448528+ansonwhc@users.noreply.github.com>
2022-04-11 15:29:46 +0100

    Corrected package name

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='artscraping',
     version='0.1.0',
-    packages=find_packages(include=['Art_Scraping', 'Art_Scraping.*'])
+    packages=find_packages(include=['Art-Scraping', 'Art-Scraping.*'])
     install_requires=[
         'bs4',
         'PIL',
